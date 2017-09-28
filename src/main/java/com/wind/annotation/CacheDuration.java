8fe48fecdf6687f8b6bfef0ae3ab797ebe3b1d34
wind <xiaoqiang_wang@163.com>
2017-09-28 20:21:52 +0800

    first-commit

---
+++ src/main/java/com/wind/annotation/CacheDuration.java
@@ -0,0 +1,15 @@
+package com.wind.annotation;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface CacheDuration {
+
+    //Sets the expire time (in seconds).
+    long duration() default 60;
+}
